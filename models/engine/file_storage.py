c3581862a5b5c7180d2a03e0fa71e357dd6c4797
Mukasa-E <e.simiyu@alustudent.com>
2025-05-28 05:05:08 -0700

    done

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""File storage engine."""
+
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+class FileStorage:
+    """Serializes instances to a JSON file and deserializes back."""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets obj in __objects with key <obj class name>.id"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file."""
+        with open(self.__file_path, 'w') as f:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects, if it exists."""
+        try:
+            with open(self.__file_path, 'r') as f:
+                objs = json.load(f)
+                for k, v in objs.items():
+                    cls_name = v["__class__"]
+                    self.__objects[k] = eval(cls_name)(**v)
+        except FileNotFoundError:
+            pass
+
