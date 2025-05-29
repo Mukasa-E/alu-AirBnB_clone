19bc511d55f3ac51c2169493154af756a3ce8337
Mukasa-E <e.simiyu@alustudent.com>
2025-05-29 11:14:39 +0300

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 import json
-import os
 from models.base_model import BaseModel
 
 
@@ -9,26 +7,27 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        return FileStorage.__objects
+    def all(self, cls=None):
+        if cls is None:
+            return self.__objects
+        return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
 
     def new(self, obj):
         key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
-        obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
-        with open(FileStorage.__file_path, 'w') as f:
+        obj_dict = {k: v.to_dict() for k, v in self.__objects.items()}
+        with open(self.__file_path, 'w') as f:
             json.dump(obj_dict, f)
 
     def reload(self):
         try:
-            with open(FileStorage.__file_path, 'r') as f:
+            with open(self.__file_path, 'r') as f:
                 obj_dict = json.load(f)
                 for key, value in obj_dict.items():
-                    class_name = value['__class__']
+                    cls_name = value['__class__']
                     del value['__class__']
-                    if class_name == 'BaseModel':
-                        FileStorage.__objects[key] = BaseModel(**value)
+                    self.__objects[key] = eval(cls_name)(**value)
         except FileNotFoundError:
             pass
